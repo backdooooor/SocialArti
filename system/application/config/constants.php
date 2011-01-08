e99a93272dc221e4131a6b7fc1876b62239043c4
backdoor <spam@mir-saratov.ru>
2011-01-08 17:38:58 +0300

    ......

--- system/application/config/constants.php
+++ system/application/config/constants.php
@@ -35,7 +35,8 @@ define('FOPEN_WRITE_CREATE', 					'ab');
 define('FOPEN_READ_WRITE_CREATE', 				'a+b');
 define('FOPEN_WRITE_CREATE_STRICT', 			'xb');
 define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
-
+define('IS_AJAX', isset($_SERVER['HTTP_X_REQUESTED_WITH']) &&
+         strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest'); 
 
 /* End of file constants.php */
 /* Location: ./system/application/config/constants.php */
